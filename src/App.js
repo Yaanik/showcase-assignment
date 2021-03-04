db7d074ce3b6e626ea567840ab0d466ae8b5b731
Yaanik <37064608+Yaanik@users.noreply.github.com>
2021-03-04 22:43:06 +0200

    Netlify build test

--- src/App.js
+++ src/App.js
@@ -1,22 +1,5 @@
-import logo from './logo.svg';
 import Home from './pages/home';
 import Navbar from './components/navbar';
-import styled from 'styled-components';
-import {colors} from './utils/colors';
-
-
-const Layout = styled.div`
-  display: flex;
-  flex-flow: column wrap;
-  justify-content: center;
-  align-self: center;
-  max-width: 1240px;
-  width: 1240px;
-  margin: auto;
-  background-color: ${colors.white};
-  color: ${colors.dark};
-  font-family: 'Open Sans',sans-serif;
-`
 
 function App() {
   return (
